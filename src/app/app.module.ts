53d4b078d30a23588b1c54026e439dc0531b6377
Caybe <cabezasmathias@gmail.com>
2020-05-01 17:16:20 +0200

    Extracting burger client and adding navigation and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiModule } from './swagger';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BurgerListComponent } from './burger-list/burger-list.component';
+import { BurgerDetailComponent } from './burger-detail/burger-detail.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerListComponent,
+    BurgerDetailComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    ApiModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
